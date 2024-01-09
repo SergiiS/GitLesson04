4b8ff8bd2d2bdbe95b70970c44553e720983b88c
sergiistepanenko <sergiystep@gmail.com>
2024-01-09 20:43:09 +0200

    Add new system out println

--- src/Lesson04GitProgram.java
+++ src/Lesson04GitProgram.java
@@ -1,5 +1,6 @@
 public class Lesson04GitProgram {
     public static void main(String[] args) {
         System.out.println("Hello world from Sergii to Git!");
+        System.out.println("Second commit to Git!");
     }
 }
